67b600de4dab7fe1928b139ff8063c2a4afedc49
Vanessa McHale <vamchale@gmail.com>
2020-06-02 17:57:59 -0500

    Fix

--- test/Golden.hs
+++ test/Golden.hs
@@ -1,14 +1,15 @@
 module Golden ( goldenTests
               ) where
 
-import qualified Data.ByteString.Lazy            as BSL
-import           Data.Text.Lazy.Encoding         (encodeUtf8)
-import           Data.Text.Prettyprinter.Doc.Ext
+import qualified Data.ByteString.Lazy          as BSL
+import           Data.Text.Lazy.Encoding       (encodeUtf8)
+import           Data.Text.Prettyprint.Doc.Ext
 import           Language.Dickinson
-import           System.FilePath                 ((-<.>))
-import           Test.Tasty
-import           Test.Tasty.Golden
+import           System.FilePath               ((-<.>))
+import           Test.Tasty                    (TestTree)
+import           Test.Tasty.Golden             (goldenVsString)
 
+goldenTests :: TestTree
 goldenTests = withDckFile "test/data/nestLet.dck"
 
 prettyBSL :: Pretty (name a) => Dickinson name a -> BSL.ByteString
