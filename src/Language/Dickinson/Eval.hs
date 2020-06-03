802fdcd6ac5184bbcb1c9bd0ac85348ec0f2cd99
Vanessa McHale <vamchale@gmail.com>
2020-06-02 21:34:53 -0500

    Eval

--- src/Language/Dickinson/Eval.hs
+++ src/Language/Dickinson/Eval.hs
@@ -3,9 +3,11 @@
 module Language.Dickinson.Eval ( EvalM
                                ) where
 
-import           Control.Monad.Except     (ExceptT, throwError)
-import           Control.Monad.State      (StateT, gets, modify)
+import           Control.Monad.Except     (ExceptT, runExceptT, throwError)
+import           Control.Monad.State      (StateT, evalStateT, gets, modify)
 import qualified Data.IntMap              as IM
+import           Data.Semigroup           (sconcat)
+import qualified Data.Text                as T
 import           Language.Dickinson.Error
 import           Language.Dickinson.Name
 import           Language.Dickinson.Type
@@ -13,7 +15,10 @@ import           Language.Dickinson.Type
 -- map to expression
 type EvalSt name a = IM.IntMap (Expression name a)
 
-type EvalM name a = ExceptT (DickinsonError name a) (StateT (EvalSt name a) IO)
+type EvalM name a = StateT (EvalSt name a) (ExceptT (DickinsonError name a) IO)
+
+runEvalSt :: EvalM name a x -> IO (Either (DickinsonError name a) x)
+runEvalSt = runExceptT . flip evalStateT mempty
 
 bindName :: Name a -> Expression Name a -> EvalM Name a ()
 bindName (Name _ (Unique u) _) e = modify (IM.insert u e)
@@ -26,4 +31,8 @@ lookupName n@(Name _ (Unique u) l) = go =<< gets (IM.lookup u)
     where go Nothing  = throwError (UnfoundName l n)
           go (Just x) = pure x
 
--- lol oulipo/to uppercase &c.
+-- TODO: clone identifiers
+evalM :: Expression Name a -> EvalM Name a T.Text
+evalM (Literal _ t) = pure t
+evalM (Concat _ es) = sconcat <$> traverse evalM es
+evalM (Var _ n)     = evalM =<< lookupName n
