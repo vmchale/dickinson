931cfee6f18466274bcd82f8b7d0ce5ffd279225
Vanessa McHale <vamchale@gmail.com>
2020-06-04 15:27:12 -0500

    better debugging?

--- src/Language/Dickinson.hs
+++ src/Language/Dickinson.hs
@@ -5,6 +5,7 @@ module Language.Dickinson ( parse
                           -- * Types
                           , Dickinson
                           , Declaration (..)
+                          , evalFile
                           -- * Reëxports from
                           -- "Data.Text.Prettyprint.Doc.Render.Text"
                           , renderLazy
@@ -13,11 +14,24 @@ module Language.Dickinson ( parse
                           , putDoc
                           ) where
 
+import           Control.Monad                         ((<=<))
+import           Data.Bifunctor                        (first)
+import           Data.ByteString.Lazy                  as BSL
+import qualified Data.Text                             as T
 import           Data.Text.Prettyprint.Doc             (Pretty (pretty))
 import           Data.Text.Prettyprint.Doc.Render.Text (putDoc, renderLazy,
                                                         renderStrict)
+import           Language.Dickinson.Error
+import           Language.Dickinson.Eval
 import           Language.Dickinson.Lexer
+import           Language.Dickinson.Name
 import           Language.Dickinson.Parser
 import           Language.Dickinson.Pretty
 import           Language.Dickinson.Rename
 import           Language.Dickinson.Type
+
+evalFile :: FilePath -> IO T.Text
+evalFile = fmap yeet . evalIO . evalExpressionM . yeet . findMain . yeet . parse <=< BSL.readFile
+
+yeet :: Show a => Either a x -> x
+yeet = either (error.show) id
