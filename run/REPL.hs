2be95d0794cef108c971fe672d81b1b0c7071543
Vanessa McHale <vamchale@gmail.com>
2020-06-15 09:41:28 -0500

    REPL m

--- run/REPL.hs
+++ run/REPL.hs
@@ -1,5 +1,13 @@
 module REPL ( dickinsonRepl
             ) where
 
+import           Language.Dickinson
+import           System.Console.Haskeline (InputT)
+
 dickinsonRepl :: IO ()
 dickinsonRepl = putStrLn "Not yet implemented."
+
+-- repl lexer?
+-- "emd> "
+
+type REPL a = InputT (EvalT IO a)
