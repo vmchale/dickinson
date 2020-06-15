6aec0a7e1b1be3b9d66b186db138021f0f6b02f7
Vanessa McHale <vamchale@gmail.com>
2020-06-15 14:24:37 -0500

    ScopeCheck

--- src/Language/Dickinson/ScopeCheck.hs
+++ src/Language/Dickinson/ScopeCheck.hs
@@ -1,29 +1,48 @@
 module Language.Dickinson.ScopeCheck ( checkScope
                                      ) where
 
-import           Control.Monad.State       (State, gets)
+import           Control.Applicative       (Alternative)
+import           Control.Monad.State       (State, evalState, get, modify)
+import           Data.Foldable             (asum)
 import qualified Data.IntSet               as IS
 import           Language.Dickinson.Error
 import           Language.Dickinson.Name
 import           Language.Dickinson.Type
 import           Language.Dickinson.Unique
 
-newtype CheckSt = CheckSt
-    { bound :: IS.IntSet }
+type CheckM = State IS.IntSet
 
-type CheckM = State CheckSt
+runCheckM :: CheckM a -> a
+runCheckM = flip evalState IS.empty
 
+insertName :: Name a -> CheckM ()
+insertName (Name _ (Unique i) _) = modify (IS.insert i)
+
+deleteName :: Name a -> CheckM ()
+deleteName (Name _ (Unique i) _) = modify (IS.delete i)
+
+-- runs after renamer
 checkScope :: Dickinson Name a -> Maybe (DickinsonError Name a)
-checkScope _ = Nothing
+checkScope = runCheckM . checkDickinson
 
 checkDickinson :: Dickinson Name a -> CheckM (Maybe (DickinsonError Name a))
-checkDickinson _ = pure Nothing
+checkDickinson = mapSumM checkDecl
+
+checkDecl :: Declaration Name a -> CheckM (Maybe (DickinsonError Name a))
+checkDecl (Define _ n e) =
+    insertName n *>
+    checkExpr e
 
 checkExpr :: Expression Name a -> CheckM (Maybe (DickinsonError Name a))
-checkExpr Literal{}  = pure Nothing
-checkExpr StrChunk{} = pure Nothing
+checkExpr Literal{}      = pure Nothing
+checkExpr StrChunk{}     = pure Nothing
+checkExpr (Interp es)    = mapSumM checkExpr es
+checkExpr (Choice _ brs) = mapSumM checkExpr (snd <$> brs)
 checkExpr (Var _ n@(Name _ (Unique i) l)) = do
-    b <- gets bound
+    b <- get
     if i `IS.member` b
         then pure Nothing
         else pure $ Just (UnfoundName l n)
+
+mapSumM :: (Traversable t, Alternative f, Applicative m) => (a -> m (f b)) -> t a -> m (f b)
+mapSumM = (fmap asum .) . traverse
