410f3969aae5fc4b5146e1453cc9558a257353ec
Vanessa McHale <vamchale@gmail.com>
2020-06-16 11:45:36 -0500

    repline?

--- run/REPL.hs
+++ run/REPL.hs
@@ -5,7 +5,7 @@ import           Control.Monad.IO.Class   (liftIO)
 import           Control.Monad.State.Lazy (StateT)
 import qualified Data.ByteString.Lazy     as BSL
 import           Language.Dickinson
-import           System.Console.Haskeline (InputT)
+import System.Console.Repline (HaskelineT)
 
 dickinsonRepl :: IO ()
 dickinsonRepl = putStrLn "Not yet implemented."
@@ -13,7 +13,7 @@ dickinsonRepl = putStrLn "Not yet implemented."
 -- repl lexer?
 -- "emd> "
 
-type Repl a = InputT (StateT (EvalSt a) IO)
+type Repl a = HaskelineT (StateT (EvalSt a) IO)
 
 
 -- eval expression OR load file
