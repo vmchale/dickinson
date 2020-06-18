9734bcc21c19dedb5f7a8c08003870f254ccb757
Vanessa McHale <vamchale@gmail.com>
2020-06-18 17:08:45 -0500

    Add some test cases

--- test/Eval.hs
+++ test/Eval.hs
@@ -3,14 +3,28 @@
 module Eval ( evalTests
             ) where
 
-import Language.Dickinson
-import Test.Tasty (TestTree)
-import Test.Tasty.HUnit (Assertion, (@?=), testCase)
+import           Language.Dickinson
+import           Test.Tasty         (TestTree, testGroup)
+import           Test.Tasty.HUnit   (Assertion, testCase, (@?=))
 
 evalTests :: TestTree
-evalTests = testCase "Should evalutate to a constant" constEval
+evalTests = testGroup "Evalutation test"
+    [ testCase "Should evalutate to a constant" constEval
+    , testCase "Should allow declarations in other orders" scopeEval
+    , testCase "Should allow higher-order functions" higherOrderEval
+    ]
 
 constEval :: Assertion
 constEval = do
     res <- evalFile "test/eval/context.dck"
     res @?= "woman"
+
+scopeEval :: Assertion
+scopeEval = do
+    res <- evalFile "test/demo/circular.dck"
+    res @?= "a"
+
+higherOrderEval :: Assertion
+higherOrderEval = do
+    res <- evalFile "test/data/higherOrder.dck"
+    res @?= "It's me"
