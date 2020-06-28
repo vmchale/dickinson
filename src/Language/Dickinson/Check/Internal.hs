5d1bef2e5d76d976a7b938382437aa2f4b837197
Vanessa McHale <vamchale@gmail.com>
2020-06-27 19:26:43 -0500

    Polish up

--- src/Language/Dickinson/Check/Internal.hs
+++ src/Language/Dickinson/Check/Internal.hs
@@ -3,12 +3,14 @@ module Language.Dickinson.Check.Internal ( sanityCheck
 
 import           Control.Monad             (when)
 import           Control.Monad.State       (MonadState)
+import           Data.Text.Prettyprint.Doc (pretty)
 import           Language.Dickinson.Name
 import           Language.Dickinson.Rename
 import           Language.Dickinson.Type
 import           Language.Dickinson.Unique
 import           Lens.Micro.Mtl            (use)
 
+-- TODO: sanity check for the lexer
 sanityCheck :: (HasRenames s, MonadState s m) => [Declaration a] -> m ()
 sanityCheck d = do
     storedMax <- use (rename.maxLens)
@@ -20,12 +22,31 @@ maxUniqueDeclaration :: Declaration a -> Int
 maxUniqueDeclaration (Define _ (Name _ (Unique i) _) e) = max i (maxUniqueExpression e)
 
 maxUniqueExpression :: Expression a -> Int
-maxUniqueExpression Literal{}                     = 0
-maxUniqueExpression StrChunk{}                    = 0
-maxUniqueExpression (Var _ (Name _ (Unique i) _)) = i
-maxUniqueExpression (Choice _ pes)                = maximum (fmap maxUniqueExpression (snd <$> pes))
-maxUniqueExpression (Interp _ es)                 = maximum (fmap maxUniqueExpression es)
-maxUniqueExpression (Concat _ es)                 = maximum (fmap maxUniqueExpression es)
-maxUniqueExpression (Apply _ e e')                = max (maxUniqueExpression e) (maxUniqueExpression e')
-maxUniqueExpression (Annot _ e _)                 = maxUniqueExpression e
-maxUniqueExpression (Flatten _ e)                 = maxUniqueExpression e
+maxUniqueExpression Literal{}                            = 0
+maxUniqueExpression StrChunk{}                           = 0
+maxUniqueExpression (Var _ (Name _ (Unique i) _))        = i
+maxUniqueExpression (Choice _ pes)                       = maximum (maxUniqueExpression . snd <$> pes)
+maxUniqueExpression (Interp _ es)                        = maximum (fmap maxUniqueExpression es)
+maxUniqueExpression (Concat _ es)                        = maximum (fmap maxUniqueExpression es)
+maxUniqueExpression (Apply _ e e')                       = max (maxUniqueExpression e) (maxUniqueExpression e')
+maxUniqueExpression (Annot _ e _)                        = maxUniqueExpression e
+maxUniqueExpression (Flatten _ e)                        = maxUniqueExpression e
+maxUniqueExpression (Tuple _ es)                         = maximum (fmap maxUniqueExpression es)
+maxUniqueExpression (Lambda _ (Name _ (Unique i) _) _ e) = max i (maxUniqueExpression e)
+maxUniqueExpression (Match _ e p e') =
+    maximum
+        [ maxUniqueExpression e
+        , maxUniquePattern p
+        , maxUniqueExpression e'
+        ]
+maxUniqueExpression (Let _ bs e) =
+    maximum
+        [ maxUniqueExpression e
+        , maximum (maxUniqueExpression . snd <$> bs)
+        , maximum (unUnique . unique . fst <$> bs)
+        ]
+
+maxUniquePattern :: Pattern a -> Int
+maxUniquePattern (PatternVar _ (Name _ (Unique i) _)) = i
+maxUniquePattern Wildcard{}                           = 0
+maxUniquePattern (PatternTuple _ ps)                  = maximum (fmap maxUniquePattern ps)
