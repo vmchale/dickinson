9be6222752dd29499eea2e83c3ba3c0eaed1c085
Vanessa McHale <vamchale@gmail.com>
2020-06-28 14:33:45 +0000

    examples

--- src/Data/Text/Prettyprint/Doc/Ext.hs
+++ src/Data/Text/Prettyprint/Doc/Ext.hs
@@ -22,7 +22,7 @@ import qualified Data.Text                             as T
 import qualified Data.Text.Lazy                        as TL
 import           Data.Text.Prettyprint.Doc             (Doc, LayoutOptions (LayoutOptions),
                                                         PageWidth (AvailablePerLine), Pretty (pretty), SimpleDocStream,
-                                                        concatWith, defaultLayoutOptions, flatAlt, hardline, indent,
+                                                        concatWith, flatAlt, hardline, indent,
                                                         layoutSmart, softline, vsep, (<+>))
 import           Data.Text.Prettyprint.Doc.Render.Text (renderLazy, renderStrict)
 
