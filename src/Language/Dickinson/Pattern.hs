7240cb9e23ad282b3ee32fa347a63da74d06d51f
Vanessa McHale <vamchale@gmail.com>
2020-07-13 16:59:44 -0500

    flip args

--- src/Language/Dickinson/Pattern.hs
+++ src/Language/Dickinson/Pattern.hs
@@ -8,14 +8,14 @@ import           Data.List.NonEmpty       as NE
 import           Language.Dickinson.Error
 import           Language.Dickinson.Type
 
-matches :: Expression a -> Pattern a -> Bool
-matches _ Wildcard{}                           = True
-matches (Constructor _ tn) (PatternCons _ tn') = tn == tn'
-matches _ PatternVar{}                         = True
-matches (Tuple _ es) (PatternTuple _ ps)       = and (NE.zipWith matches es ps) -- already check they're the same length during amalgamation
+matches :: Pattern a -> Expression a -> Bool
+matches Wildcard{} _                           = True
+matches (PatternCons _ tn') (Constructor _ tn) = tn == tn'
+matches PatternVar{} _                         = True
+matches (PatternTuple _ ps) (Tuple _ es)       = and (NE.zipWith matches ps es) -- already check they're the same length during amalgamation
 matches _ _                                    = False
 
 matchPattern :: MonadError (DickinsonError a) m => a -> Expression a -> [(Pattern a, Expression a)] -> m (Pattern a, Expression a)
-matchPattern l e (p:ps) | matches e (fst p) = pure p
+matchPattern l e (p:ps) | matches (fst p) e = pure p
                         | otherwise = matchPattern l e ps
 matchPattern l e [] = throwError $ PatternFail l e
