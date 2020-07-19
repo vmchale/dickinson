8b786aaa4220152593186f9db1decb001e40d8e7
Vanessa McHale <vamchale@gmail.com>
2020-07-19 12:49:06 -0500

    let

--- src/Control/Monad/Ext.hs
+++ src/Control/Monad/Ext.hs
@@ -7,7 +7,7 @@ import           Data.List.NonEmpty
 import qualified Data.List.NonEmpty as NE
 
 zipWithM :: (Applicative m) => (a -> b -> m c) -> NonEmpty a -> NonEmpty b -> m (NonEmpty c)
-zipWithM f xs ys =  sequenceA (NE.zipWith f xs ys)
+zipWithM f xs ys = sequenceA (NE.zipWith f xs ys)
 
 zipWithM_ :: (Applicative m) => (a -> b -> m c) -> NonEmpty a -> NonEmpty b -> m ()
 zipWithM_ f xs ys = sequenceA_ (NE.zipWith f xs ys)
