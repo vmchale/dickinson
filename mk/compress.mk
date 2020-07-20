f9f7e390152b33ce44524b3ea0275c79409ec4f4
Vanessa McHale <vamchale@gmail.com>
2020-07-19 21:55:38 -0500

    for source dist?

--- mk/compress.mk
+++ mk/compress.mk
@@ -2,15 +2,12 @@
 
 %.zst: %
 	sak compress $< $@ --best
-	chmod +x $@
 
 %.gz: %
 	gzip -k -f $< --best
 
 %.lz: %
 	sak compress $< $@ --best
-	chmod +x $@
 
 %.bz2: %
 	sak compress $< $@ --best
-	chmod +x $@
