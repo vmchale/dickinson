ff42b5aaa2a56d5fb58cd0833ac17ce9722f577a
Vanessa McHale <vanessa.mchale@bitnomial.com>
2020-08-10 17:11:33 -0500

    declAdd

--- src/Language/Dickinson/Pattern/Useless.hs
+++ src/Language/Dickinson/Pattern/Useless.hs
@@ -1,8 +1,7 @@
--- see
 module Language.Dickinson.Pattern.Useless ( useful
                                           ) where
 
-import           Control.Monad             (zipWithM)
+import           Control.Monad             (forM_, zipWithM)
 import           Control.Monad.State       (State, get)
 import           Data.Foldable             (toList)
 import           Data.IntMap               (findWithDefault)
@@ -14,13 +13,27 @@ import           Data.Maybe                (mapMaybe)
 import           Language.Dickinson.Name
 import           Language.Dickinson.Type
 import           Language.Dickinson.Unique
+import           Lens.Micro                (Lens')
+import           Lens.Micro.Mtl            (modifying)
 
 -- all constructors of a
-data PatternEnv a = PatternEnv { allCons :: IM.IntMap IS.IntSet -- ^ all constructors indexed by type
-                               , types   :: IM.IntMap Int -- ^ all types indexed by constructor
-                               }
+data PatternEnv = PatternEnv { allCons :: IM.IntMap IS.IntSet -- ^ all constructors indexed by type
+                             , types   :: IM.IntMap Int -- ^ all types indexed by constructor
+                             }
 
-type PatternM a = State (PatternEnv a)
+allConsLens :: Lens' PatternEnv (IM.IntMap IS.IntSet)
+allConsLens f s = fmap (\x -> s { allCons = x }) (f (allCons s))
+
+typesLens :: Lens' PatternEnv (IM.IntMap Int)
+typesLens f s = fmap (\x -> s { types = x }) (f (types s))
+
+declAdd :: Declaration a -> PatternM ()
+declAdd Define{}         = pure ()
+declAdd (TyDecl l tn@(Name _ (Unique i) _) cs) =
+    forM_ cs $ \(Name _ (Unique j) _) ->
+        modifying typesLens (IM.insert j i)
+
+type PatternM = State PatternEnv
 
 isWildcard :: Pattern a -> Bool
 isWildcard Wildcard{}   = True
@@ -33,7 +46,8 @@ extrCons = concat . mapMaybe g where
     g (OrPattern _ ps)  = Just $ extrCons (toList ps)
     g _                 = Nothing
 
-assocUniques :: Name a -> PatternM a IS.IntSet
+-- given a constructor name, get the IntSet of all constructors of that type
+assocUniques :: Name a -> PatternM IS.IntSet
 assocUniques (Name _ (Unique i) _) = do
     st <- get
     let ty = findWithDefault (error "Internal error in pattern-match coverage checker.") i (types st)
@@ -48,7 +62,7 @@ columinzeExt _                   = error "Pattern-match coverage checker does no
 columinzeBlock :: [Pattern a] -> [[Pattern a]]
 columinzeBlock = transpose . fmap columinzeExt
 
-isExhaustive :: [Pattern a] -> PatternM a Bool
+isExhaustive :: [Pattern a] -> PatternM Bool
 isExhaustive ps                       | any isWildcard ps = pure True
 isExhaustive ps@((PatternCons _ c):_) = do
     let allUniques = unUnique . unique <$> extrCons ps
@@ -57,7 +71,7 @@ isExhaustive ps@((PatternCons _ c):_) = do
 isExhaustive ps@(PatternTuple{}:_)    = allA isExhaustive (columinzeBlock ps)
 isExhaustive ((OrPattern _ ps):ps')   = isExhaustive (toList ps ++ ps')
 
-useful :: [Pattern a] -> Pattern a -> PatternM a Bool
+useful :: [Pattern a] -> Pattern a -> PatternM Bool
 useful [] _                    = pure True
 useful ps (OrPattern _ ps')    = anyA (useful ps) ps' -- all?
 useful ps PatternTuple{}       | any isWildcard ps = pure False -- so we can call columinzeBlock without fear
