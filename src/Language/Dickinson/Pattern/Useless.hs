7c30d6082d8d98f14fb46611d260cd30317c24a8
Vanessa McHale <vamchale@gmail.com>
2020-08-19 15:45:00 -0500

    Fix up pattern match exhaustiveness checker

--- src/Language/Dickinson/Pattern/Useless.hs
+++ src/Language/Dickinson/Pattern/Useless.hs
@@ -12,13 +12,12 @@ module Language.Dickinson.Pattern.Useless ( PatternM
 import           Control.Monad             (forM, forM_)
 import           Control.Monad.State       (State, evalState, get)
 import           Data.Foldable             (toList, traverse_)
+import           Data.Functor              (void)
 import           Data.IntMap               (findWithDefault)
 import qualified Data.IntMap               as IM
 import qualified Data.IntSet               as IS
 import           Data.List.Ext
 import           Data.List.NonEmpty        (NonEmpty ((:|)))
-import qualified Data.List.NonEmpty        as NE
-import           Data.Maybe                (mapMaybe)
 import           Language.Dickinson.Name
 import           Language.Dickinson.Type
 import           Language.Dickinson.Unique
@@ -69,7 +68,7 @@ isCompleteSet ns@(n:_) = do
     let ty = unUnique . unique <$> ns
     pure $
         if IS.null (allU IS.\\ IS.fromList ty)
-            then Just ((\u -> Name ("(internal)" :| []) (Unique u) ()) <$> IS.toList allU)
+            then Just ((\u -> (Name ("(internal)" :| []) (Unique u) ())) <$> IS.toList allU)
             else Nothing
 
 useful :: [Pattern a] -> Pattern a -> PatternM Bool
@@ -84,22 +83,56 @@ specializeTag c = concatMap withRow
                                         | otherwise = []
           withRow (PatternTuple{}:_)    = sanityFailed
           withRow (Wildcard{}:ps)       = [ps]
+          withRow (PatternVar{}:ps)     = [ps]
           withRow (OrPattern _ rs:ps)   = specializeTag c [r:ps | r <- toList rs] -- TODO: unit test case for this
+          withRow []                    = emptySpecialize
 
--- TODO: unit test these to make sure they make sense w.r.t. dimensions & such?
 specializeTuple :: Int -> [[Pattern a]] -> [[Pattern a]]
 specializeTuple n = concatMap withRow
     where withRow (PatternTuple _ ps:ps') = [toList ps ++ ps']
           withRow (p@Wildcard{}:ps')      = [replicate n p ++ ps']
+          withRow (p@PatternVar{}:ps')    = [replicate n p ++ ps']
           withRow (OrPattern _ rs:ps)     = specializeTuple n [r:ps | r <- toList rs]
           withRow (PatternCons{}:_)       = sanityFailed
+          withRow []                      = emptySpecialize
+
+emptySpecialize :: a
+emptySpecialize = error "Internal error: tried to take specialized matrix of an empty row"
 
 -- | \\( \matcal(D) \\) in the Maranget paper
 defaultMatrix :: [[Pattern a]] -> [[Pattern a]]
 defaultMatrix = concatMap withRow where
-    withRow (PatternTuple{}:_) = []
-    withRow (PatternCons{}:_)  = []
-    withRow (Wildcard{}:ps)    = [ps]
+    withRow []                  = error "Internal error: tried to take default matrix of an empty row"
+    withRow (PatternTuple{}:_)  = error "Sanity check failed!" -- because a tuple would be complete by itself
+    withRow (PatternCons{}:_)   = []
+    withRow (Wildcard{}:ps)     = [ps]
+    withRow (PatternVar{}:ps)   = [ps]
+    withRow (OrPattern _ rs:ps) = defaultMatrix [r:ps | r <- toList rs]
+
+data Complete a = NotComplete
+                | CompleteTuple Int
+                | CompleteTags [Name a]
+
+extrCons :: Pattern a -> [Name a]
+extrCons (PatternCons _ c) = [c]
+extrCons (OrPattern _ ps)  = concatMap extrCons (toList ps)
+extrCons _                 = []
+
+-- Is the first column of the pattern matrix complete?
+fstComplete :: [[Pattern a]] -> PatternM (Complete ())
+fstComplete ps =
+    if maxTupleLength > 0
+        then pure $ CompleteTuple maxTupleLength
+        else do
+            res <- isCompleteSet (concatMap extrCons fstColumn)
+            pure $ case res of
+                Just ns -> CompleteTags ns
+                Nothing -> NotComplete
+    where fstColumn = fmap head ps
+          tuple (PatternTuple _ ps') = length ps'
+          tuple (OrPattern _ ps')    = maximum (tuple <$> ps')
+          tuple _                    = 0
+          maxTupleLength = maximum (tuple <$> fstColumn)
 
 -- follows maranget paper
 usefulMaranget :: [[Pattern a]] -> [Pattern a] -> PatternM Bool
@@ -108,3 +141,21 @@ usefulMaranget _ []                       = pure False
 usefulMaranget ps (PatternCons _ c:qs)    = usefulMaranget (specializeTag c ps) qs
 usefulMaranget ps (PatternTuple _ ps':qs) = usefulMaranget (specializeTuple (length ps') ps) (toList ps' ++ qs)
 usefulMaranget ps (OrPattern _ ps':qs)    = forAnyA ps' $ \p -> usefulMaranget ps (p:qs)
+usefulMaranget ps (q@Wildcard{}:qs)       = do
+    cont <- fstComplete ps
+    case cont of
+        NotComplete     -> usefulMaranget (defaultMatrix ps) qs
+        CompleteTuple n -> usefulMaranget (specializeTuple n ps) (specializeTupleVector n q qs)
+        CompleteTags ns -> or <$> (forM ns $ \n -> usefulMaranget (specializeTag n (forget ps)) (fmap void qs))
+usefulMaranget ps (q@PatternVar{}:qs)     = do
+    cont <- fstComplete ps
+    case cont of
+        NotComplete     -> usefulMaranget (defaultMatrix ps) qs
+        CompleteTuple n -> usefulMaranget (specializeTuple n ps) (specializeTupleVector n q qs)
+        CompleteTags ns -> or <$> (forM ns $ \n -> usefulMaranget (specializeTag n (forget ps)) (fmap void qs))
+
+specializeTupleVector :: Int -> Pattern a -> [Pattern a] -> [Pattern a]
+specializeTupleVector n p ps = replicate n p ++ ps
+
+forget :: [[Pattern a]] -> [[Pattern ()]]
+forget = fmap (fmap void)
