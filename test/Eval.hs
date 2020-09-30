1ee2401964b2cf0103ed1c425677a4a2bbcb38a6
Vanessa McHale <vamchale@gmail.com>
2020-09-30 14:49:57 -0500

    eval test case

--- test/Eval.hs
+++ test/Eval.hs
@@ -34,6 +34,7 @@ evalTests = testGroup "Evaluation test"
     , resultCase "test/error/scoping.dck"
     , resultCase "examples/divinationBot.dck"
     -- , resultCase "test/error/mutual.dck"
+    , resultCase "test/eval/chooseHigherOrder.dck""
     ]
 
 forceResult :: a -> Assertion
