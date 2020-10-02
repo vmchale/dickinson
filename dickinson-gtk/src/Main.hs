ed7cfb0f0992c378e87e0e5eccb15adc8ed412d3
Vanessa McHale <vamchale@gmail.com>
2020-10-02 17:13:12 -0500

    Files

--- dickinson-gtk/src/Main.hs
+++ dickinson-gtk/src/Main.hs
@@ -6,18 +6,17 @@ module Main (main) where
 
 import           Data.Functor                  (void)
 import qualified Data.Text                     as T
-import           GI.Gtk                        (Label (..), Window (..), windowTitle)
-import           GI.Gtk.Declarative            (Attribute ((:=)), bin, notebook, page, widget)
+import           GI.Gtk                        (Button (..), Window (..), windowTitle)
+import           GI.Gtk.Declarative            (Attribute ((:=)), bin, notebook, on, page, widget)
 import           GI.Gtk.Declarative.App.Simple (App (..), AppView, Transition, run)
 
 -- https://haskell-at-work.com/episodes/2018-11-13-gtk-programming-with-haskell.html
--- http://book.realworldhaskell.org/read/gui-programming-with-gtk-hs.html
 -- https://github.com/owickstrom/gi-gtk-declarative/blob/master/examples/MenuBar.hs
 
 data State = Inp !T.Text
            | None
 
-data Event = ReadText
+data Event = Step
            | Run
 
 view' :: State -> AppView Window Event
@@ -27,7 +26,7 @@ view' _ = bin Window
     , #heightRequest := 300
     ] $
         notebook []
-            [ page "(new)" $ widget Label [#label := "Work"]
+            [ page "(new)" $ widget Button [#label := "Run", on #clicked Run]
             ]
 
 update' :: State -> Event -> Transition State Event
