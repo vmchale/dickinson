4208cf2475ad09374d36a1d0878c1d0f08323d54
Vanessa McHale <vamchale@gmail.com>
2021-10-10 14:10:28 -0500

    scaffold textbox

--- dickinson-fltk/src/Main.hs
+++ dickinson-fltk/src/Main.hs
@@ -1,5 +1,18 @@
-module Main ( main
-            ) where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import           Data.Functor                       (void)
+import qualified Graphics.UI.FLTK.LowLevel.FL       as FL
+import           Graphics.UI.FLTK.LowLevel.FLTKHS
+import           Graphics.UI.FLTK.LowLevel.Fl_Types
 
 main :: IO ()
-main = pure ()
+main = do
+  win <- doubleWindowNew (toSize (640,480)) Nothing (Just "Simple Fl_Text_Editor")
+  buff <- textBufferNew Nothing Nothing
+  edit <- textEditorNew (toRectangle (20,20,(640-40),(480-40))) Nothing
+  setBuffer edit (Just buff)
+  showWidget win
+  setText buff "%-\n(:def main \"Hello, World!\""
+  void $ FL.run
